257d2bdb9f50757411ad57b29884273b21a7eac3
Iuliia <iuliiaponomareva@gmail.com>
2020-03-05 23:41:50 +0300

    Fix counters

--- AccountService/src/main/java/julia/accountservice/statistics/StatisticsService.java
+++ AccountService/src/main/java/julia/accountservice/statistics/StatisticsService.java
@@ -6,51 +6,87 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 @Service
 public class StatisticsService {
     public static final String COUNTER_AMOUNT_GET_TOTAL = "counter.amount.get.total";
     public static final String COUNTER_AMOUNT_ADD_TOTAL = "counter.amount.add.total";
     public static final String COUNTER_AMOUNT_GET_SECOND = "counter.amount.get.second";
     public static final String COUNTER_AMOUNT_ADD_SECOND = "counter.amount.add.second";
-    private AtomicLong totalGetCounter = new AtomicLong(0);
-    private AtomicLong getCounter = new AtomicLong(0);
-    private AtomicLong totalAddCounter = new AtomicLong(0);
-    private AtomicLong addCounter = new AtomicLong(0);
+    private final Object getLock = new Object();
+    private final Object addLock = new Object();
+    private long totalGetCounter = 0L;
+    private long getCounter = 0L;
+    private long totalAddCounter = 0L;
+    private long addCounter = 0L;
 
     @Autowired
     public StatisticsService(MeterRegistry registry) {
-        Gauge.builder(COUNTER_AMOUNT_GET_SECOND, getCounter, AtomicLong::get)
+        Gauge.builder(COUNTER_AMOUNT_GET_SECOND, this::getGetCounter)
                 .register(registry);
-        Gauge.builder(COUNTER_AMOUNT_GET_TOTAL, totalGetCounter, AtomicLong::get)
+        Gauge.builder(COUNTER_AMOUNT_GET_TOTAL, this::getTotalGetCounter)
                 .register(registry);
-        Gauge.builder(COUNTER_AMOUNT_ADD_SECOND, addCounter, AtomicLong::get)
+        Gauge.builder(COUNTER_AMOUNT_ADD_SECOND, this::getAddCounter)
                 .register(registry);
-        Gauge.builder(COUNTER_AMOUNT_ADD_TOTAL, totalAddCounter, AtomicLong::get)
+        Gauge.builder(COUNTER_AMOUNT_ADD_TOTAL, this::getTotalAddCounter)
                 .register(registry);
+    }
+
+    public long getGetCounter() {
+        synchronized (getLock) {
+            return getCounter;
+        }
+    }
+
+    public long getTotalGetCounter() {
+        synchronized (getLock) {
+            return totalGetCounter;
+        }
+    }
+
+    public long getAddCounter() {
+        synchronized (addLock) {
+            return addCounter;
+        }
+    }
+
+    public long getTotalAddCounter() {
+        synchronized (addLock) {
+            return totalAddCounter;
+        }
     }
 
     public void updateGetAmountCounter() {
-        totalGetCounter.getAndIncrement();
-        getCounter.getAndIncrement();
+        synchronized (getLock) {
+            totalGetCounter++;
+            getCounter++;
+        }
     }
 
-    public void updateAddAmountCounter() {
-        totalAddCounter.getAndIncrement();
-        addCounter.getAndIncrement();
+    public void updateAddAmountCounter()  {
+        synchronized (addLock) {
+            totalAddCounter++;
+            addCounter++;
+        }
     }
 
     @Scheduled(fixedRate = 1000)
     private void updateStatsPerSecond() {
-        getCounter.set(0);
-        addCounter.set(0);
+        synchronized (getLock) {
+            getCounter = 0L;
+        }
+        synchronized (addLock) {
+            addCounter = 0L;
+        }
     }
 
     public void reset() {
-        totalAddCounter.set(0);
-        totalGetCounter.set(0);
-        getCounter.set(0);
-        addCounter.set(0);
+        synchronized (getLock) {
+            totalGetCounter = 0L;
+            getCounter = 0L;
+        }
+        synchronized (addLock) {
+            totalAddCounter = 0L;
+            addCounter = 0L;
+        }
     }
 }
